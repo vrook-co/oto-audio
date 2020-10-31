d571b21a6d919ca1cd904cb4704817da14eec6b4
varunramesh26 <varunramesh.07@gmail.com>
2020-10-31 09:16:14 +0530

    added pytest & setup modules

---
+++ oto-audio-lib/pytest.py
@@ -0,0 +1,15 @@
+"""
+Here is the pytest module, write the test cases for the modules to be tested.
+Please complete it by using the required packages or modules from the oto-helper library, and the logic for it to work.
+"""
+
+# Add the import statements below
+
+
+# Write the functions definitions that are required for this module below
+def test_case_name(**args, **kwargs):
+    # Please add the docstrings for this function
+
+def test_passed(**args, **kwargs):
+    # Please add the doctrings for this function
+
---
+++ oto-audio-lib/setup.py
@@ -0,0 +1,15 @@
+"""
+Here is the setup module for packaging the audio library.
+Please complete it by using the required packages or modules from the oto-helper library, and the logic for it to work.
+"""
+
+# Add the import statements below
+
+
+# Write the functions definitions that are required for this module below
+def library_info(**args, **kwargs):
+    # Please add the docstrings for this function
+
+def library_urls(**args, **kwargs):
+    # Please add the doctrings for this function
+
